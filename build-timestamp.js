356794be5903539deeb46f04937a81d472aacb9b
Marc Michelmann <marc@evalurate.net>
2025-08-14 00:50:57 +0800

    🔄 Force Vercel rebuild with updated build timestamp

--- build-timestamp.js
+++ build-timestamp.js
@@ -1,16 +1,5 @@
-// Build timestamp file - changes on every build to force Vercel to rebuild
-const timestamp = Date.now();
-const buildId = `build-${timestamp}`;
-const hash = Math.random().toString(36).substring(2, 15);
+// Build timestamp to force rebuilds
+export const BUILD_TIMESTAMP = '2025-08-13-23:50:00';
 
-console.log(`Build timestamp: ${timestamp}`);
-console.log(`Build ID: ${buildId}`);
-console.log(`Build hash: ${hash}`);
-
-// Export for use in build process
-module.exports = {
-  timestamp,
-  buildId,
-  hash,
-  forceRebuild: true
-};
+// Force rebuild when this file changes
+export const FORCE_REBUILD = true;
